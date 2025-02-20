bf5f3cf53f5c6ecedd91a7ee2b22cd5a93b2e413
ted <ted1998917@gmail.com>
2025-02-20 15:12:16 +0800

    try add embed_chatbot seperately

---
+++ public/embed_chatbot.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue';
+import ChatBotContent from '@/components/ChatBotContent.vue';
+
+const app = createApp(ChatBotContent);
+app.mount('#embed-chatbot');
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import './assets/main.css'
 import 'intl-tel-input/build/css/intlTelInput.css';
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import ChatBotContent from './components/ChatBotContent.vue';
 import App from './App.vue'
 import router from './router'
 
@@ -12,3 +12,11 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+// if (window.location.pathname.startsWith('/embed_chatbot')) {
+//     // If it's an embed route, mount EmbeddableChatbot directly
+//     createApp(ChatBotContent).mount('#app');
+//   } else {
+//     // For other routes, mount the default App component as usual
+//     app.mount('#app');
+//   }
